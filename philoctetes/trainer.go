ed593e834f2959efe28742cc053e6adc02d60f96
Alonso Vidales <alonso.vidales@tras2.es>
2015-10-22 23:56:30 +0200

    Working on the training models

--- philoctetes/trainer.go
+++ philoctetes/trainer.go
@@ -16,33 +16,18 @@ import (
 	"github.com/alonsovidales/go_ml"
 	"github.com/alonsovidales/pit/log"
 	"github.com/alonsovidales/v/charont"
+	"math"
 	"os"
-	//"sort"
 	"strings"
 	"time"
 )
 
-type Result struct {
-	ProfitByTime float64
-	Profit       float64
-	Time         int64
-	ThrendOnBuy  float64
-	ThrendOnSell float64
-	AverageBuy   float64
-	AverageSell  float64
-	PriceOnBuy   float64
-	PriceOnSell  float64
-}
-
-type resultsByCurrencyTy []*Result
-
-func (a resultsByCurrencyTy) Len() int           { return len(a) }
-func (a resultsByCurrencyTy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a resultsByCurrencyTy) Less(i, j int) bool { return a[i].ProfitByTime > a[j].ProfitByTime }
+const (
+	cToStart = 100
+)
 
 type Trainer struct {
-	feeds map[string][]charont.CurrVal
-	//results          map[string]resultsByCurrencyTy
+	feeds            map[string][]charont.CurrVal
 	windowSize       int
 	logRegModelsBuy  map[string]*ml.Regression
 	logRegModelsSell map[string]*ml.Regression
@@ -97,10 +82,8 @@ func GetTrainer(trainingFile string, TimeRangeToStudySecs int64, windowSize int)
 
 func (fd *Trainer) studyCurrencies(TimeRangeToStudySecs int64) {
 	log.Debug("Studing currencies...")
-	//fd.results = make(map[string]resultsByCurrencyTy)
 	toFinish := len(fd.feeds)
 	for curr, prices := range fd.feeds {
-		//fd.results[curr] = resultsByCurrencyTy{}
 		// Prepare the dataset for the logixtic regression estimation
 		fd.logRegModelsBuy[curr] = &ml.Regression{
 			LinearReg: false,
@@ -114,54 +97,83 @@ func (fd *Trainer) studyCurrencies(TimeRangeToStudySecs int64) {
 		}
 		go func(curr string, prices []charont.CurrVal) {
 			log.Debug("Studing currency:", curr)
+			studiesPointsSell := make(map[int]bool)
 
-			for i, price := range prices {
-
-				// Calculate the threads and previous conditions before
-				// buy
-				threndOnBuy := 0.0
-				avgOnBuy := 0.0
-				var prevPrice charont.CurrVal
-				from := i - fd.windowSize
-				if from < 0 {
-					from = 0
-				}
-				windowRange := prices[from:i]
-				for i, priceWindow := range windowRange {
-					avgOnBuy += priceWindow.Ask
-					if i != 0 {
-						threndOnBuy += priceWindow.Ask / prevPrice.Ask
-					}
-					prevPrice = priceWindow
-				}
-				threndOnBuy /= float64(len(windowRange) - 1)
-				avgOnBuy /= float64(len(windowRange))
-
-				maxBenef := 0.0
+			for i, price := range prices[cToStart:] {
+				maxBenef := math.Inf(-1)
 				maxBenefPoint := 0
-				minBenef := 0.0
+				minBenef := math.Inf(1)
 				minBenefPoint := 0
 				for j := i + 1; j < len(prices) && price.Ts+TimeRangeToStudySecs > prices[j].Ts; j++ {
-					benef := prices[j].Bid/price.Ask - 1
-					if benef < 0 && minBenef > benef {
-						minBenef = benef
+					//benefTime := (price.Bid / prices[j].Ask) / (float64(prices[j].Ts-price.Ts) / 1000000000)
+					benefTime := (price.Bid / prices[j].Ask)
+
+					if benefTime < minBenef {
+						minBenef = benefTime
 						minBenefPoint = j
 					}
-					if benef > 0 && maxBenef < benef {
-						maxBenef = benef
+					if benefTime > maxBenef {
+						maxBenef = benefTime
 						maxBenefPoint = j
 					}
 				}
 
-				if minBenefPoint != 0 {
-					fd.addWindowInfo(curr, prices, price, minBenefPoint, threndOnBuy, avgOnBuy)
+				if _, ok := studiesPointsSell[minBenefPoint]; ok {
+					continue
 				}
-				if maxBenefPoint != 0 {
-					fd.addWindowInfo(curr, prices, price, maxBenefPoint, threndOnBuy, avgOnBuy)
+				if _, ok := studiesPointsSell[maxBenefPoint]; ok {
+					continue
+				}
+				studiesPointsSell[minBenefPoint] = true
+				studiesPointsSell[maxBenefPoint] = true
+
+				askThrend, askAvg, askMin, askMax, askVar, askCovar := fd.GetInfoSection(prices[i:cToStart+i], true)
+				sellThrend, sellAvg, sellkMin, sellMax, sellVar, sellCovar := fd.GetInfoSection(prices[maxBenefPoint:cToStart+maxBenefPoint], false)
+				minSellThrend, minSellAvg, minSellkMin, minSellMax, minSellVar, minSellCovar := fd.GetInfoSection(prices[minBenefPoint:cToStart+minBenefPoint], false)
+				fd.logRegModelsBuy[curr].X = append(
+					fd.logRegModelsBuy[curr].X,
+					[]float64{
+						price.Ask / askAvg, askThrend, askAvg, askMin, askMax, askVar, askCovar,
+					},
+				)
+				fd.logRegModelsSell[curr].X = append(
+					fd.logRegModelsSell[curr].X,
+					[]float64{
+						price.Ask / askAvg, askThrend, askAvg, askMin, askMax, askVar, askCovar,
+						price.Bid / sellAvg, sellThrend, sellAvg, sellkMin, sellMax, sellVar, sellCovar,
+					},
+				)
+				fd.logRegModelsSell[curr].Y = append(
+					fd.logRegModelsSell[curr].Y,
+					1,
+				)
+				fd.logRegModelsSell[curr].X = append(
+					fd.logRegModelsSell[curr].X,
+					[]float64{
+						price.Ask / askAvg, askThrend, askAvg, askMin, askMax, askVar, askCovar,
+						price.Bid / minSellAvg, minSellThrend, minSellAvg, minSellkMin, minSellMax, minSellVar, minSellCovar,
+					},
+				)
+				fd.logRegModelsSell[curr].Y = append(
+					fd.logRegModelsSell[curr].Y,
+					0,
+				)
+				if maxBenef > 0 {
+					// We have a winner!
+					fd.logRegModelsBuy[curr].Y = append(
+						fd.logRegModelsBuy[curr].Y,
+						1,
+					)
+				} else {
+					// Nope :'(
+					fd.logRegModelsBuy[curr].Y = append(
+						fd.logRegModelsBuy[curr].Y,
+						0,
+					)
 				}
 			}
 
-			log.Debug("Trainig model for currency:", curr)
+			log.Debug("Trainig model for currency:", curr, "Buy:", len(fd.logRegModelsBuy[curr].X), len(fd.logRegModelsBuy[curr].Y), "Sell:", len(fd.logRegModelsSell[curr].X), len(fd.logRegModelsSell[curr].Y))
 			fd.logRegModelsSell[curr].InitializeTheta()
 			fd.logRegModelsBuy[curr].InitializeTheta()
 			ml.Fmincg(fd.logRegModelsSell[curr], 0.0, 1000, true)
@@ -175,85 +187,59 @@ func (fd *Trainer) studyCurrencies(TimeRangeToStudySecs int64) {
 	for toFinish > 0 {
 		time.Sleep(time.Second)
 	}
-
-	/*for curr, values := range fd.results {
-		sort.Sort(values)
-		for _, v := range values[:10] {
-			log.Debug("CURR:", curr, v.ProfitByTime)
-		}
-	}*/
 }
 
-func (fd *Trainer) addWindowInfo(curr string, prices []charont.CurrVal, price charont.CurrVal, j int, threndOnBuy, avgOnBuy float64) {
-	benef := prices[j].Bid/price.Ask - 1
-	//rangeSecs := float64(prices[j].Ts-price.Ts) / 1000000000
+func (fd *Trainer) GetInfoSection(prices []charont.CurrVal, ask bool) (thrend, avg, min, max, variance, covariance float64) {
+	var val, prev float64
+	points := float64(len(prices))
+	max = math.Inf(1)
 
-	//log.Debug("Benef in Time:", curr, i, j, "Secs:", rangeSecs, "=", benef, "T:", benef/rangeSecs, price, prices[j])
-	// The benef by Time is going to be the score
-
-	//benefTime := benef / rangeSecs
-
-	var prevPrice charont.CurrVal
-
-	threndOnSell := 0.0
-	avgOnSell := 0.0
-	first := true
-	fromSell := j - fd.windowSize
-	if fromSell < 0 {
-		fromSell = 0
-	}
-	windowRange := prices[fromSell:j]
-	for _, priceWindow := range windowRange {
-		avgOnSell += priceWindow.Bid
-		if !first {
-			threndOnSell += priceWindow.Bid / prevPrice.Bid
-			prevPrice = priceWindow
+	for i, p := range prices {
+		if ask {
+			val = p.Ask
+		} else {
+			val = p.Bid
 		}
-		first = false
-	}
-	threndOnSell /= float64(len(windowRange) - 1)
-	avgOnSell /= float64(len(windowRange))
 
-	fd.logRegModelsBuy[curr].X = append(
-		fd.logRegModelsBuy[curr].X,
-		[]float64{1.0000, avgOnBuy / price.Ask, threndOnBuy},
-	)
-	fd.logRegModelsSell[curr].X = append(
-		fd.logRegModelsSell[curr].X,
-		[]float64{1.0000, avgOnSell / prices[j].Bid, avgOnSell / avgOnBuy, threndOnBuy, threndOnSell},
-	)
-	valToAdd := 0.0
-	if benef > 0 {
-		valToAdd = 1
-	}
-	fd.logRegModelsBuy[curr].Y = append(
-		fd.logRegModelsBuy[curr].Y,
-		valToAdd,
-	)
-	fd.logRegModelsSell[curr].Y = append(
-		fd.logRegModelsSell[curr].Y,
-		valToAdd,
-	)
+		if i != 0 {
+			thrend += val / prev
+		}
+		avg += val
+		if min > val {
+			min = val
+		}
+		if max < val {
+			max = val
+		}
+		avg += val
 
-	//log.Debug(benefTime)
-	/*fd.results[curr] = append(fd.results[curr], &Result{
-		Profit:       benef,
-		ProfitByTime: benefTime,
-		Time:         int64(rangeSecs) * 1000000000,
-		PriceOnBuy:   price.Ask,
-		PriceOnSell:  prices[j].Bid,
-		ThrendOnBuy:  threndOnBuy,
-		AverageBuy:   avgOnBuy,
-		ThrendOnSell: threndOnSell,
-		AverageSell:  avgOnSell,
-	})*/
+		prev = val
+	}
+
+	avg /= points
+	thrend /= points - 1
+
+	for _, p := range prices {
+		if ask {
+			val = p.Ask
+		} else {
+			val = p.Bid
+		}
+
+		variance += math.Pow(val-avg, 2)
+	}
+
+	covariance = math.Sqrt(variance)
+
+	return
 }
 
 func (fd *Trainer) ShouldIBuy(curr string, threndOnBuy, averageBuy, priceOnBuy float64) bool {
-	hip := fd.logRegModelsBuy[curr].LogisticHipotesis([]float64{1, averageBuy / priceOnBuy, threndOnBuy})
-	log.Debug("Hip:", curr, hip)
+	//hip := fd.logRegModelsBuy[curr].LogisticHipotesis([]float64{1, averageBuy / priceOnBuy, threndOnBuy})
+	//log.Debug("Hip:", curr, hip)
 
-	return hip > 0.5
+	//return hip > 0.5
+	return false
 }
 
 func (fd *Trainer) GetPredictionToSell(Profit float64, Time int64, ThrendOnBuy, ThrendOnSell, AverageBuy, AverageSell, PriceOnBuy, PriceOnSell float64) (pred float64) {
